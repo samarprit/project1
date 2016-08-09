2840a2c1980c680b632a40ab6a8779c2ebba34c9
codecademy <ccuser@codecademy.com>
2016-08-09 14:37:03 -0400

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#welcome'
+  get '/pages/portfolio' => 'pages#portfolio'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
